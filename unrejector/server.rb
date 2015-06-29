1aadb0fb8454a13fa1d45c1df702e7ec94614308
Zach Latta <zach@zachlatta.com>
2015-06-29 15:07:12 -0700

    unrejector: redirect http traffic to https in production.
    closes #129.

--- unrejector/server.rb
+++ unrejector/server.rb
@@ -6,6 +6,8 @@ require 'stripe'
 ENV['RACK_ENV'] == 'test' ? require('fakeredis') : require('redis')
 
 class StaticPageServer < Sinatra::Base
+  use Rack::SslEnforcer, only_environments: 'production'
+
   set :stripe_publishable_key, ENV['STRIPE_PUBLISHABLE_KEY']
   set :stripe_secret_key, ENV['STRIPE_SECRET_KEY']
   set :drive_client_id, ENV['DRIVE_CLIENT_ID']
