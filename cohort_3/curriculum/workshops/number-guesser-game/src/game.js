a2e0fef09ade425642dc225c6d69b621e4152e93
Max Wofford <max@hackedu.us>
2015-07-22 00:27:59 -0700

    cohort 3: [number-guesser-game workshop] While loop that tests guess() does not have a double negative

--- cohort_3/curriculum/workshops/number-guesser-game/src/game.js
+++ cohort_3/curriculum/workshops/number-guesser-game/src/game.js
@@ -10,15 +10,15 @@ function guess(correctAnswer){
     alert('Too low!');
   } else if(userGuess == correctAnswer){
     alert('You won!');
-    return true;
+    return false;
   }
-  return false;
+  return true;
 }
 
 function startGame(){
   var answer = getRandomNumber(20);
   var score = 1;
-  while(guess(answer) != true){
+  while(guess(answer)){
     score = score + 1;
   }
   alert('You guessed the number in ' + score + ' turns.');
