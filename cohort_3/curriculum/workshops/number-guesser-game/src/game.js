20c99b34b22b203d7093d7839249c9018fe69315
Max Wofford <max@hackedu.us>
2015-07-22 00:10:17 -0700

    cohort 3: [number-guesser-game workshop] Reworked to be more beginner-friendly

--- cohort_3/curriculum/workshops/number-guesser-game/src/game.js
+++ cohort_3/curriculum/workshops/number-guesser-game/src/game.js
@@ -1,25 +1,25 @@
-function getRandomNumber(min, max){
-  return Math.floor(Math.random() * (max - min + 1) + min);
+function getRandomNumber(maximum){
+  return Math.round(Math.random() * maximum);
 }
 
 function guess(correctAnswer){
   var userGuess = prompt('Make a guess!');
-  if (userGuess > correctAnswer)
+  if(userGuess > correctAnswer){
     alert('Too high!');
-  else if (userGuess < correctAnswer)
+  } else if(userGuess < correctAnswer){
     alert('Too low!');
-  else if (userGuess == correctAnswer){
+  } else if(userGuess == correctAnswer){
     alert('You won!');
-    return true
+    return true;
   }
-  return false
+  return false;
 }
 
 function startGame(){
-  var answer = getRandomNumber(1, 20);
+  var answer = getRandomNumber(20);
   var score = 1;
-  while (guess(answer) != true){
-    score ++;
+  while(guess(answer) != true){
+    score = score + 1;
   }
   alert('You guessed the number in ' + score + ' turns.');
 }
