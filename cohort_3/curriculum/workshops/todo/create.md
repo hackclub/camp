696c7fe409c3d7b554733b36e913e414851cec32
Jonathan Leung <me@jonl.org>
2015-07-22 20:22:07 -0700

    cohort 3: Add todo workshop

---
+++ cohort_3/curriculum/workshops/todo/create.md
@@ -0,0 +1,42 @@
+# Creating The Project
+
+By now, you should know how to create 3 base files of a web application.
+
+```md
+Challenge!  
+Time        __
+           / _)   
+    .-^^^-/ /
+ __/       /
+<__.|_|-|_|
+
+1) Create these 3 files
+- index.html
+- main.js
+- main.css
+
+2) Then link them with the appropriate tags.
+```
+
+## Pro Tips
+
+### Use Shortcut Keys
+
+**Get Cloud9 to automatically give you the HTML scaffold by:**
+
+- opening up `.html` file
+- typing in `!`
+- pressing `tab`
+
+![](http://zippy.gfycat.com/HappygoluckyMintyChevrotain.gif)
+
+**Automatically generate the link and script tags**
+
+- type `script`
+- then the tab key
+- type link
+- then the tab key
+
+![](http://zippy.gfycat.com/ExhaustedMasculineCow.gif)
+
+Next: [Challenge Solution](create_implementation.md).
---
+++ cohort_3/curriculum/workshops/todo/src/attempt_1/6_firebase_delete/main.js
@@ -0,0 +1,105 @@
+var FIREBASE_ROOT = "https://camp-todo.firebaseio.com/"
+
+window.onload = function() {
+
+  var firebase = new Firebase(FIREBASE_ROOT);
+  var newButton = document.getElementById("new-button");
+  var todoList = document.getElementById("todo-list")
+
+  function createTask(taskDescription) {
+    var task = {}
+    task.description = taskDescription;
+    task.isDone = false;
+    
+    firebase.push(task);
+  }
+
+  function deleteTask(key) {
+    var taskURL = FIREBASE_ROOT + key;
+    var taskRef = new Firebase(FIREBASE_ROOT + key);
+
+    taskRef.remove();
+  }
+
+  function updateTask(key, isDone) {
+    var taskUrl = FIREBASE_ROOT + key;
+    var taskRef = new Firebase(FIREBASE_ROOT + key);
+    
+    var data = {
+      isDone: isDone
+    }
+
+    taskRef.update(data);
+  }
+
+  function createTaskElement(description, isDone, key) {
+    var li = document.createElement("li");
+    li.setAttribute("key", key);
+
+    var input = document.createElement("input");
+    input.setAttribute("class", "toggle");
+    input.setAttribute("type", "checkbox");
+    input.checked = isDone;
+
+    input.onchange = function() {
+      var isDone = this.checked
+      var li = this.parentElement;
+      var key = li.getAttribute("key");
+
+      updateTask(key, isDone);
+    }
+
+    var span = document.createElement("span");
+    span.innerHTML = description;
+
+    var del = document.createElement("img");
+    del.src = "img/delete.png";
+    del.setAttribute("class", "delete");
+
+    del.onclick = function() {
+      var li = this.parentElement;
+      var key = li.getAttribute("key");
+
+      deleteTask(key);
+    }
+
+    li.appendChild(input);
+    li.appendChild(span);
+    li.appendChild(del);
+
+    return li
+  }
+
+  newButton.onclick = function() {
+    var taskDescription = prompt("Add a task:");
+    if (taskDescription !== null && taskDescription !== "") {
+      createTask(taskDescription);
+    }
+  }
+
+  firebase.on('child_added', function (snapshot) {
+    var task = snapshot.val();
+    var key = snapshot.key();
+    var taskElement = createTaskElement(task.description, task.isDone, key);
+    todoList.appendChild(taskElement);
+
+  });
+
+  firebase.on('child_changed', function (snapshot) {
+    var task = snapshot.val();
+    var key = snapshot.key();
+
+    var li = document.querySelector("[key=" + key + "]");
+    var checkbox = li.childNodes[0];
+    checkbox.checked = task.isDone;
+  });
+
+  firebase.on('child_removed', function (snapshot) {
+    var key = snapshot.key();
+    var taskElement = document.querySelector("[key=" + key + "]");
+    todoList.removeChild(taskElement);
+  });
+
+
+
+}
