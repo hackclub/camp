9e3884ff5407b7d14b0809074071d7d55815eacd
Jonathan Leung <me@jonl.org>
2015-07-22 23:07:54 -0700

    add code versions

---
+++ cohort_3/curriculum/workshops/todo/readme.md
@@ -0,0 +1,3 @@
+# Building a Todo List
+
+By the end of this tutorial, you will learn how to build this todo list:
---
+++ cohort_3/curriculum/workshops/todo/src/4_edit_firebase/main.js
@@ -0,0 +1,81 @@
+var FIREBASE_ROOT = "https://camp-todo.firebaseio.com/"
+
+window.onload = function() {
+
+  var firebase = new Firebase(FIREBASE_ROOT);
+  var newButton = document.getElementById("new-button");
+  var todoList = document.getElementById("todo-list")
+
+  function updateTask(key, isDone) {
+    var taskUrl = FIREBASE_ROOT + key;
+    var taskRef = new Firebase(FIREBASE_ROOT + key);
+    
+    var data = {
+      isDone: isDone
+    }
+
+    taskRef.update(data);
+  }
+
+  function createTaskElement(description, isDone, key) {
+    var li = document.createElement("li");
+    li.setAttribute("key", key);
+
+    var input = document.createElement("input");
+    input.setAttribute("class", "toggle");
+    input.setAttribute("type", "checkbox");
+    input.checked = isDone;
+
+    input.onchange = function() {
+      var isDone = this.checked
+      var li = this.parentElement;
+      var key = li.getAttribute("key");
+
+      updateTask(key, isDone);
+    }
+
+    var span = document.createElement("span");
+    span.innerHTML = description;
+
+    li.appendChild(input);
+    li.appendChild(span)
+
+    return li
+  }
+
+  function addTaskToList(description, isDone, key) {
+    var taskElement = createTaskElement(description, isDone, key);
+    todoList.appendChild(taskElement);
+  }
+
+  function createTask() {
+    var taskDescription = prompt("Add a task:");
+
+    if (taskDescription !== null && taskDescription !== "") {
+      var task = {}
+      task.description = taskDescription;
+      task.isDone = false;
+      
+      firebase.push(task);
+    }
+  }
+  
+  newButton.onclick = createTask
+
+  firebase.on('child_added', function (snapshot) {
+    var task = snapshot.val();
+    addTaskToList(task.description, task.isDone, snapshot.key());
+  });
+
+  firebase.on('child_changed', function (snapshot) {
+    var task = snapshot.val();
+    var key = snapshot.key();
+
+    var li = document.querySelector("[key=" + key + "]")
+    var checkbox = li.childNodes[0];
+    checkbox.checked = task.isDone;
+  })
+
+
+
+}
