ded6ff09e1f8f934f2091701f6b68901f3255a85
Jonathan Leung <me@jonl.org>
2015-07-22 20:31:23 -0700

    can edit task completion status

---
+++ cohort_3/curriculum/workshops/todo/4_firebase_edit/main.js
@@ -0,0 +1,52 @@
+var firebase = new Firebase("https://camp-todo.firebaseio.com/");
+
+function createTaskElement(description, isDone) {
+
+  var li = document.createElement("li");
+
+  var input = document.createElement("input");
+  input.setAttribute("class", "toggle");
+  input.setAttribute("type", "checkbox");
+  input.checked = isDone;
+
+  var span = document.createElement("span");
+  span.innerHTML = description;
+
+  li.appendChild(input);
+  li.appendChild(span)
+
+  return li
+}
+
+function main() {
+
+  var newButton = document.getElementById("new-button");
+  var todoList = document.getElementById("todo-list")
+
+  function writeTask(description, isDone) {
+    var taskElement = createTaskElement(description, isDone);
+    todoList.appendChild(taskElement);
+  }
+
+  function createTask() {
+    var taskDescription = prompt("Add a task:");
+
+    if (taskDescription !== null && taskDescription !== "") {
+      var task = {}
+      task.description = taskDescription;
+      task.isDone = false;
+      
+      firebase.push(task);
+    }
+  }
+  
+  newButton.onclick = createTask
+
+  firebase.on('child_added', function (snapshot) {
+    var task = snapshot.val();
+    writeTask(task.description, task.isDone);
+  });
+
+}
+
+window.onload = main;
